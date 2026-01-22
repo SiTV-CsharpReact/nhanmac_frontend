0c9167fb5fad92519734ccecbcb69439a4ef2c0c
SiTV-CsharpReact <vansi19a@gmail.com>
2026-01-22 22:07:47 +0700

    update_new

--- next.config.js
+++ next.config.js
@@ -1,22 +1,28 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+    reactStrictMode: false,
     images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "backend.nhanmac.vn",
-                pathname: "/upload/**",
-            },
-            {
-                protocol: "https",
-                hostname: "luattiendat.com.vn",
-                pathname: "/wp-content/**",
-            },
-        ],
+        
+      domains: ['nhanmac.vn', 'localhost', 'luattiendat.com.vn', 'frontend.nhanmac.vn', 'backend.nhanmac.vn'],
+      remotePatterns: [
+        {
+            
+          protocol: 'http',
+          hostname: 'localhost',
+          port: '3600',
+          pathname: '/uploads/**',
+          
+        },
+      ],
     },
-};
-
-module.exports = nextConfig;
+    
+  };
+  
+  module.exports = nextConfig;
